21a0de959b90cffcf920f0829a619c686ff98836
Mykhailo Kuzich <timmkedth@gmail.com>
2022-05-28 20:23:31 +0300

    1, 2 task complated

--- JS/01-task.js
+++ JS/01-task.js
@@ -5,3 +5,18 @@
 // вывод fizzbuzz вместо чисел, кратных как 3, так и 5.
 
 
+function numbers(n) {
+    for (let i = 1; i <= n; i += 1) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+numbers(20);
--- JS/02-task.js
+++ JS/02-task.js
@@ -3,3 +3,12 @@
 
 // Постановка
 // Нужно написать функцию, которая проверяет, являются ли две строки анаграммами, причем регистр букв не имеет значения. Учитываются лишь символы; пробелы или знаки препинания в расчет не берутся.
+
+function anagramCheck(a, b) {
+    const reversedB = b.split('').reverse().join('');
+    const result = a.toLowerCase() === reversedB.toLowerCase() ? 'It is anagram.' : 'It is NOT anagram!'
+    console.log(result);
+}
+
+anagramCheck('Bla', 'Alb');
+anagramCheck('Bla', 'bla');
